56960efea079a3237d958d2a121cded2dd6f75ae
matyashelena <avrora.solar@gmail.com>
2023-09-11 20:47:28 +0300

    add data types and operators

--- 15_JS/dist/main.dev.js
+++ 15_JS/dist/main.dev.js
@@ -2,7 +2,13 @@
 
 function userName() {
   var userName = prompt('Add your name');
-  alert("Hello, ".concat(userName, "!"));
+
+  if (!isNaN(userName)) {
+    alert('Рlease enter a name using letters!');
+  } else {
+    alert("Hello, ".concat(userName, "!"));
+  }
+
   console.log(userName);
 }
 
@@ -10,11 +16,15 @@ function userAge() {
   var userBirthday = parseInt(prompt('Add your year of birth'));
   var currentYear = 2023;
 
-  if (userBirthday > currentYear) {
-    alert("You weren't born yet!");
+  if (!isNaN(userBirthday)) {
+    if (userBirthday > currentYear) {
+      alert("You weren't born yet!");
+    } else {
+      var userAge = currentYear - userBirthday;
+      alert("You're ".concat(userAge, " years old!"));
+    }
   } else {
-    var userAge = currentYear - userBirthday;
-    alert("You're ".concat(userAge, " years old!"));
+    alert('Please enter a numerical value!');
   }
 
   console.log(userAge);
@@ -23,14 +33,26 @@ function userAge() {
 function perimeterSquare() {
   var size = parseInt(prompt('Add the length of the side of the square'));
   var perimeterSquare = size * 4;
-  alert("Perimeter of the square ".concat(perimeterSquare));
+
+  if (isNaN(perimeterSquare)) {
+    alert('Please enter a numerical value!');
+  } else {
+    alert("Perimeter of the square ".concat(perimeterSquare));
+  }
+
   console.log(perimeterSquare);
 }
 
 function areaCircle() {
   var radiusCircle = prompt('Enter the radius of the circle');
   var areaCircle = Math.round(2 * Math.PI * radiusCircle);
-  alert("The area of the circle ".concat(areaCircle));
+
+  if (isNaN(radiusCircle)) {
+    alert('Please enter a numerical value!');
+  } else {
+    alert("The area of the circle ".concat(areaCircle));
+  }
+
   console.log(areaCircle);
 }
 
@@ -38,7 +60,13 @@ function userSpeed() {
   var distance = prompt('Add the distance in kilometers between the two cities');
   var time = prompt('How many hours you want to get there');
   var speed = (distance / time).toFixed(2);
-  alert("You need to move at a speed of ".concat(speed, " km/h to be on time"));
+
+  if (!isNaN(distance) && !isNaN(time)) {
+    alert("You need to move at a speed of ".concat(speed, " km/h to be on time"));
+  } else {
+    alert('Please enter a numerical value!');
+  }
+
   console.log(speed);
 }
 
@@ -48,6 +76,83 @@ function currencyConverter() {
 
   var dollars = prompt('Enter the amount in dollars');
   var result = (rate * dollars).toFixed(2);
-  alert("You can get ".concat(result, " euros"));
+
+  if (isNaN(dollars)) {
+    alert('Please enter a numerical value!');
+  } else {
+    alert("You can get ".concat(result, " \u20AC"));
+    console.log(result);
+  }
+}
+
+function correctAnswer() {
+  var a = 0.1;
+  var b = 0.2;
+  var result = parseInt((a + b) * 10) / 10;
+  alert("A mathematically correct answer is ".concat(result));
   console.log(result);
+}
+
+function correctAnswerString() {
+  var a = 1;
+  var b = '2';
+  var result = a + +b;
+  alert("A mathematically correct answer is ".concat(result));
+  console.log(result);
+}
+
+function numberFiles() {
+  var sizeFlash = prompt('Enter the volume of the flash drive in GB');
+  var gB = 1024;
+  var sizeFile = 820;
+  var numberFiles = parseInt(sizeFlash * gB / sizeFile);
+
+  if (isNaN(sizeFlash)) {
+    alert('Please enter a numerical value!');
+  } else {
+    alert("".concat(numberFiles));
+    console.log(numberFiles);
+  }
+}
+
+function chocolates() {
+  var money = prompt('Enter the amount of money in the wallet');
+  var price = prompt('Enter the price of one chocolate bar');
+  var chocolates = parseInt(money / price);
+  var change = money % price;
+
+  if (!isNaN(money) && !isNaN(price)) {
+    alert("You can buy ".concat(chocolates, " chocolates, the change is ").concat(change));
+  } else {
+    alert('Please enter a numerical value!');
+  }
+}
+
+function backwardsNumber() {
+  var number = prompt('Enter a three-digit number');
+  var a = parseInt(number % 1000 / 100).toString();
+  var b = parseInt(number % 100 / 10).toString();
+  var c = parseInt(number % 10).toString();
+  var result = +(c + b + a);
+
+  if (!isNaN(number) && number.length == 3) {
+    alert("".concat(result));
+  } else {
+    alert('Please enter a three-digit number!');
+  }
+}
+
+function amountDeposit() {
+  var amount = prompt('Enter the amount of the deposit');
+  var interestMonth = 0.12;
+  var daysYear = 365;
+  var month = 2;
+  var days = month * 30.4;
+  var result = (amount * interestMonth / daysYear * days).toFixed(2);
+
+  if (!isNaN(amount)) {
+    alert("Amount of accrued interest ".concat(result));
+  } else {
+    alert('Please enter a numerical value!');
+  }
 }
--- 15_JS/main.js
+++ 15_JS/main.js
@@ -1,17 +1,26 @@
 function userName() {
   const userName = prompt('Add your name');
-  alert(`Hello, ${userName}!`);
+  if (!isNaN(userName)) {
+    alert('Рlease enter a name using letters!');
+  } else {
+    alert(`Hello, ${userName}!`);
+  }
+
   console.log(userName);
 }
 
 function userAge() {
   const userBirthday = parseInt(prompt('Add your year of birth'));
   const currentYear = 2023;
-  if (userBirthday > currentYear) {
-    alert(`You weren't born yet!`);
+  if (!isNaN(userBirthday)) {
+    if (userBirthday > currentYear) {
+      alert(`You weren't born yet!`);
+    } else {
+      var userAge = currentYear - userBirthday;
+      alert(`You're ${userAge} years old!`);
+    }
   } else {
-    var userAge = currentYear - userBirthday;
-    alert(`You're ${userAge} years old!`);
+    alert('Please enter a numerical value!');
   }
   console.log(userAge);
 }
@@ -19,14 +28,22 @@ function userAge() {
 function perimeterSquare() {
   const size = parseInt(prompt('Add the length of the side of the square'));
   var perimeterSquare = size * 4;
-  alert(`Perimeter of the square ${perimeterSquare}`);
+  if (isNaN(perimeterSquare)) {
+    alert('Please enter a numerical value!');
+  } else {
+    alert(`Perimeter of the square ${perimeterSquare}`);
+  }
   console.log(perimeterSquare);
 }
 
 function areaCircle() {
   let radiusCircle = prompt('Enter the radius of the circle');
   let areaCircle = Math.round(2 * Math.PI * radiusCircle);
-  alert(`The area of the circle ${areaCircle}`);
+  if (isNaN(radiusCircle)) {
+    alert('Please enter a numerical value!');
+  } else {
+    alert(`The area of the circle ${areaCircle}`);
+  }
   console.log(areaCircle);
 }
 
@@ -34,7 +51,11 @@ function userSpeed() {
   let distance = prompt('Add the distance in kilometers between the two cities');
   let time = prompt('How many hours you want to get there');
   let speed = (distance / time).toFixed(2);
-  alert(`You need to move at a speed of ${speed} km/h to be on time`);
+  if (!isNaN(distance) && !isNaN(time)) {
+    alert(`You need to move at a speed of ${speed} km/h to be on time`);
+  } else {
+    alert('Please enter a numerical value!');
+  }
   console.log(speed);
 }
 
@@ -42,6 +63,78 @@ function currencyConverter() {
   const rate = 0.93; /*exchange rate dollar to euro*/
   var dollars = prompt('Enter the amount in dollars');
   var result = (rate * dollars).toFixed(2);
-  alert(`You can get ${result} euros`);
+  if (isNaN(dollars)) {
+    alert('Please enter a numerical value!');
+  } else {
+    alert(`You can get ${result} €`);
+    console.log(result);
+  }
+}
+
+function correctAnswer() {
+  let a = 0.1;
+  let b = 0.2;
+  let result = parseInt((a + b) * 10) / 10;
+  alert(`A mathematically correct answer is ${result}`);
   console.log(result);
+}
+
+function correctAnswerString() {
+  let a = 1;
+  let b = '2';
+  let result = a + +b;
+  alert(`A mathematically correct answer is ${result}`);
+  console.log(result);
+}
+
+function numberFiles() {
+  let sizeFlash = prompt('Enter the volume of the flash drive in GB');
+  const gB = 1024;
+  const sizeFile = 820;
+  let numberFiles = parseInt((sizeFlash * gB) / sizeFile);
+  if (isNaN(sizeFlash)) {
+    alert('Please enter a numerical value!');
+  } else {
+    alert(`${numberFiles}`);
+    console.log(numberFiles);
+  }
+}
+
+function chocolates() {
+  let money = prompt('Enter the amount of money in the wallet');
+  let price = prompt('Enter the price of one chocolate bar');
+  let chocolates = parseInt(money / price);
+  let change = money % price;
+  if (!isNaN(money) && !isNaN(price)) {
+    alert(`You can buy ${chocolates} chocolates, the change is ${change}`);
+  } else {
+    alert('Please enter a numerical value!');
+  }
+}
+
+function backwardsNumber() {
+  let number = prompt('Enter a three-digit number');
+  let a = parseInt(number % 1000 / 100).toString();
+  let b = parseInt(number % 100 / 10).toString();
+  let c = parseInt(number % 10).toString();
+  let result = +(c + b + a);
+  if (!isNaN(number) && (number.length == 3)) {
+    alert(`${result}`);
+  } else {
+    alert('Please enter a three-digit number!');
+  }
+}
+
+function amountDeposit() {
+  let amount = prompt('Enter the amount of the deposit');
+  const interestMonth = 0.12;
+  const daysYear = 365;
+  let month = 2;
+  let days = month*30.4;
+  let result = (amount*interestMonth/daysYear*days).toFixed(2);
+  if (!isNaN(amount)) {
+    alert(`Amount of accrued interest ${result}`);
+  } else {
+    alert('Please enter a numerical value!');
+  }
 }
