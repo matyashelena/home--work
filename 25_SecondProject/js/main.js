84a17e562714bf1d4f6ff081130c12ef1281fdd3
matyashelena <avrora.solar@gmail.com>
2023-10-15 19:27:40 +0300

    Add Second  Project

---
+++ 25_SecondProject/js/main.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+  $('.hero_slider-vertical').slick({
+    // dots: true,
+    infinite: true,
+    // speed: 1000,
+    // fade: true,
+    // vertical: true,
+    // verticalSwiping: true,
+    // cssEase: 'linear',
+    // arrows:false,
+    // dots: true,
+    // autoplay: true,
+    // speed: 3000,
+    // autoplaySpeed: 2000,
+    dots: true,
+    vertical: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    verticalSwiping: true,
+  });
+});
