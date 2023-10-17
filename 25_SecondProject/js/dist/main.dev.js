a55096c8be3afe4533146d62ea4eed84cf8f6353
matyashelena <avrora.solar@gmail.com>
2023-10-17 09:45:53 +0300

    add gallery&news section

--- 25_SecondProject/js/dist/main.dev.js
+++ 25_SecondProject/js/dist/main.dev.js
@@ -20,4 +20,35 @@ $(document).ready(function () {
     slidesToScroll: 1,
     verticalSwiping: true
   });
+  $('.news_cards').slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [{
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    }, {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    }, {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    } // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+    ]
+  });
 });
