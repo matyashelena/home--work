abf120e9474c2fa35fc67876b841384de49a50ba
matyashelena <avrora.solar@gmail.com>
2023-10-18 17:24:16 +0300

    add.section.contact

--- 25_SecondProject/js/dist/main.dev.js
+++ 25_SecondProject/js/dist/main.dev.js
@@ -1,6 +1,10 @@
 "use strict";
 
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
 $(document).ready(function () {
+  var _$$slick;
+
   $('.hero_slider-vertical').slick({
     // dots: true,
     infinite: true,
@@ -20,39 +24,88 @@ $(document).ready(function () {
     slidesToScroll: 1,
     verticalSwiping: true
   });
-  $('.news_cards').slick({
+  $('.news_cards').slick((_$$slick = {
     dots: true,
-    infinite: false,
+    infinite: true,
     speed: 300,
     slidesToShow: 3,
     slidesToScroll: 1,
-    responsive: [{
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        infinite: true,
-        dots: true
-      }
-    }, {
-      breakpoint: 768,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 1
-      }
-    }, {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    } // You can unslick at a given breakpoint now by adding:
-    // settings: "unslick"
-    // instead of a settings object
-    ]
-  }); // $('#lightgallery').lightGallery({
+    autoplay: true
+  }, _defineProperty(_$$slick, "speed", 1000), _defineProperty(_$$slick, "autoplaySpeed", 4000), _defineProperty(_$$slick, "responsive", [{
+    breakpoint: 1024,
+    settings: {
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      infinite: true,
+      dots: true
+    }
+  }, {
+    breakpoint: 768,
+    settings: {
+      slidesToShow: 2,
+      slidesToScroll: 1
+    }
+  }, {
+    breakpoint: 480,
+    settings: {
+      slidesToShow: 1,
+      slidesToScroll: 1
+    }
+  } // You can unslick at a given breakpoint now by adding:
+  // settings: "unslick"
+  // instead of a settings object
+  ]), _$$slick)); // $('#lightgallery').lightGallery({
   //   plugins: [lgZoom, lgThumbnail],
   // });
+
+  var scrollToSection = function scrollToSection() {
+    var el = $(this);
+    var dest = el.attr('href'); // получаем направление
+
+    if (dest !== undefined && dest !== '') {
+      // проверяем существование
+      $('html').animate({
+        scrollTop: $(dest).offset().top // прокручиваем страницу к требуемому элементу
+
+      }, 1000 // скорость прокрутки
+      );
+    }
+
+    return false;
+  };
+
+  $('.header_menu-link').on("click", scrollToSection);
+  $('.scroll_down').on("click", scrollToSection);
+  $('.scrool_map').on("click", scrollToSection);
+  var element = document.getElementById('hero').getBoundingClientRect(); // let positionInfo = element.
+
+  var height = element.height - 80;
+  console.log(element);
+  console.log(height); // let element = document.getElementById('hero').getBoundingClientRect();
+  // let height = element.height / 5;
+
+  $(window).on("scroll", function () {
+    if ($(window).scrollTop() > height) {
+      $(".header").addClass("active"); // console.log(window.screenTop());
+    } else {
+      //remove the background property so it comes transparent again (defined in your css)
+      $(".header").removeClass("active");
+    }
+  });
+  $('.map').on('click', function initMap() {
+    var map = L.map('my-map').setView([50.00317690860705, 36.226601536338116], 15);
+    L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OSM</a> contributors'
+    }).addTo(map);
+    var customIcon = L.icon({
+      iconUrl: './img/marker.svg',
+      iconSize: [106, 106] // size of the icon
+
+    });
+    L.marker([50.00181420905747, 36.23288158444743], {
+      icon: customIcon
+    }).addTo(map).bindPopup('My Lovely Location').openPopup(); // $('.map').off('click', function initMap());
+  });
 });
 lightGallery(document.getElementById('lightgallery'), {
   plugins: [lgZoom, lgThumbnail],
@@ -61,19 +114,9 @@ lightGallery(document.getElementById('lightgallery'), {
 
 }); // MAP
 // const customIcon = L.icon({
-//     iconBorderRadius: [50],
-//     iconUrl: 'https://static-00.iconduck.com/assets.00/map-pin-icon-384x512-m24sswd5.png',
-//     iconSize:     [106, 106], // size of the icon
-//     iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
-//     popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+//   iconBorderRadius: [50],
+//   iconUrl: 'https://static-00.iconduck.com/assets.00/map-pin-icon-384x512-m24sswd5.png',
+//   iconSize: [106, 106], // size of the icon
+//   iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+//   popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
 // });
-// const map = L.map('my-map').setView([50.00317690860705, 36.226601536338116], 15);
-// L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
-//     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OSM</a> contributors'
-// }).addTo(map);
-// L.marker([50.00317690860705, 36.226601536338116], {icon: customIcon}).addTo(map)
-//     .bindPopup('My Marker')
-//     .openPopup();
-// L.marker([50.00317690860705, 36.226601536338116], {icon: customIcon}).addTo(map)
-//     .bindPopup('My Marker 2')
-//     .openPopup();
--- 25_SecondProject/js/main.js
+++ 25_SecondProject/js/main.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
   $('.hero_slider-vertical').slick({
     // dots: true,
     infinite: true,
@@ -21,12 +21,14 @@ $(document).ready(function() {
 
   $('.news_cards').slick({
     dots: true,
-    infinite: false,
+    infinite: true,
     speed: 300,
     slidesToShow: 3,
     slidesToScroll: 1,
-    responsive: [
-      {
+    autoplay: true,
+    speed: 1000,
+    autoplaySpeed: 4000,
+    responsive: [{
         breakpoint: 1024,
         settings: {
           slidesToShow: 3,
@@ -59,63 +61,68 @@ $(document).ready(function() {
   //   plugins: [lgZoom, lgThumbnail],
   // });
 
-  $('.header_menu-link').click(function() {
-    const scrollName = $(this).attr('href');
-    const scrollElem = $(scrollName);
-    const scrollTop = scrollElem.offset().top;
-
-$('html, body').animate({
-  scrollTop: scrollTop
-}, 1000)
-  });
-
-  $('.scroll_down').on( 'click', function(){ 
+  let scrollToSection = function() {
     var el = $(this);
     var dest = el.attr('href'); // получаем направление
-    if(dest !== undefined && dest !== '') { // проверяем существование
-        $('html').animate({ 
-    	    scrollTop: $(dest).offset().top // прокручиваем страницу к требуемому элементу
+    if (dest !== undefined && dest !== '') { // проверяем существование
+      $('html').animate({
+          scrollTop: $(dest).offset().top // прокручиваем страницу к требуемому элементу
         }, 1000 // скорость прокрутки
-        );
+      );
     }
     return false;
-});
-let element = document.getElementById('hero').getBoundingClientRect();
-// let positionInfo = element.
-let height = element.height;
-console.log(height);
+  };
 
-$(function() {
-  // let element = document.getElementById('hero').getBoundingClientRect();
-  // let height = element.height / 5;
+  $('.header_menu-link').on("click", scrollToSection);
 
-  $(window).on("scroll", function() {
-      if($(window).scrollTop() > height) {
-          $(".header").addClass("active");
-          // console.log(window.screenTop());
+  $('.scroll_down').on("click", scrollToSection);
+
+  $('.scrool_map').on("click", scrollToSection);
+
+  let element = document.getElementById('hero').getBoundingClientRect();
+  // let positionInfo = element.
+  let height = element.height - 80;
+  console.log(element);
+  console.log(height);
+
+  
+    // let element = document.getElementById('hero').getBoundingClientRect();
+    // let height = element.height / 5;
+
+    $(window).on("scroll", function () {
+      if ($(window).scrollTop() > height) {
+        $(".header").addClass("active");
+        // console.log(window.screenTop());
       } else {
-          //remove the background property so it comes transparent again (defined in your css)
-         $(".header").removeClass("active");
+        //remove the background property so it comes transparent again (defined in your css)
+        $(".header").removeClass("active");
       }
   });
-});
 
-$('.map').on( 'click', function(){
+  $('.map').on('click', function initMap() {
 
-  const map = L.map('my-map').setView([50.00317690860705, 36.226601536338116], 15);
+    const map = L.map('my-map').setView([50.00317690860705, 36.226601536338116], 15);
 
-L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OSM</a> contributors'
-}).addTo(map);
+    L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OSM</a> contributors'
+    }).addTo(map);
+    
 
-L.marker([50.00317690860705, 36.226601536338116], {icon: customIcon}).addTo(map)
-    .bindPopup('My Marker')
-    .openPopup();
-L.marker([50.00317690860705, 36.226601536338116], {icon: customIcon}).addTo(map)
-    .bindPopup('My Marker 2')
-    .openPopup();
-
-});
+    let customIcon = L.icon({
+      iconUrl: './img/marker.svg',
+  
+      iconSize:     [106, 106], // size of the icon
+      
+  });
+    L.marker([50.00181420905747, 36.23288158444743], {
+        icon: customIcon
+      }).addTo(map)
+      .bindPopup('My Lovely Location')
+      .openPopup();
+      
+      // $('.map').off('click', function initMap());
+  });
+  
 
 
 });
@@ -129,12 +136,11 @@ lightGallery(document.getElementById('lightgallery'), {
 });
 
 // MAP
-const customIcon = L.icon({
-    iconBorderRadius: [50],
-    iconUrl: 'https://static-00.iconduck.com/assets.00/map-pin-icon-384x512-m24sswd5.png',
+// const customIcon = L.icon({
+//   iconBorderRadius: [50],
+//   iconUrl: 'https://static-00.iconduck.com/assets.00/map-pin-icon-384x512-m24sswd5.png',
 
-    iconSize:     [106, 106], // size of the icon
-    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
-    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
-});
-
+//   iconSize: [106, 106], // size of the icon
+//   iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+//   popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+// });
